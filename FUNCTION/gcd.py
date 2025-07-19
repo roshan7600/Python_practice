1ac543a362dfd85c96b73ba1b64fe7d093ec7c7a
roshan7600 <roshanpatel31296@gamil.com>
2025-07-19 18:29:31 +0530

    gcd of number

---
+++ FUNCTION/gcd.py
@@ -0,0 +1,20 @@
+# def gcd(a, b, c):
+#     gcd = min(a, b, c)  # Use min for optimization
+#     while gcd > 0:
+#         if a % gcd == 0 and b % gcd == 0 and c % gcd == 0:
+#             return gcd
+#         gcd -= 1
+#     return 1
+
+# print(gcd(10, 20, 30))  # Output: 10
+
+
+# EFFICIENT VERSION
+
+from math import gcd
+from functools import reduce
+
+def gcd_of_three(a, b, c):
+    return reduce(gcd, (a, b, c))
+
+print(gcd_of_three(10, 20, 30))  # Output: 10
