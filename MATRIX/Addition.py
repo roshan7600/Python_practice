c9f83adc9b830342ff627741218917478d866a4a
roshan7600 <roshanpatel31296@gmail.com>
2025-08-31 20:20:59 +0530

    change the code

--- MATRIX/Addition.py
+++ MATRIX/Addition.py
@@ -3,7 +3,7 @@ m2=[[0,1],[2,1]]
 
 if (len(m1)!=0 or len(m2)!=0) and (len(m1)==len(m2)) and len(m1[0])==len(m2[0]):
     for ind1 in range(len(m1)):
-        for ind2 in range(len(m2)):
+        for ind2 in range(len(m1[0])):
             m1[ind1][ind2]+=m2[ind1][ind2]
     else:
         print(m1)        
